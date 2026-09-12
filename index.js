1ce226c3657373f52958a6112111c15e22de65e1
svshsayarnia-ship-it <svshsayarnia@gmail.com>
2026-09-12 14:52:13 +0330

    chore: refresh voice client cache

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ async function serveRoot(request, response, requestUrl) {
   }
   const html = await upstream.text();
   const themed = html
-    .replace('</head>', '<link rel="stylesheet" href="/world-shell.css?v=one-world-4"></head>')
-    .replace('</body>', '<script defer src="/world-shell.js?v=one-world-4"></script></body>');
+    .replace('</head>', '<link rel="stylesheet" href="/world-shell.css?v=one-world-5"></head>')
+    .replace('</body>', '<script defer src="/world-shell.js?v=one-world-5"></script></body>');
   response.end(themed);
 }
 
