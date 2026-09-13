da19bb2483ce46e1344c8160d98e58d3525f5486
svshsayarnia-ship-it <svshsayarnia@gmail.com>
2026-09-13 10:15:03 +0330

    chore(kaykha): deploy complete command and bazaar experience

--- index.js
+++ index.js
@@ -245,6 +245,7 @@ async function proxy(request, response) {
   response.end('Not found');
 }
 
+// Bazaar reference assets are routed above.
 module.exports = async function handler(request, response) {
   try {
     await proxy(request, response);
